4c0587a4b18ef7bff4696452930232d13fc8347f
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-10-03 23:48:12 +0900

    [add] フォルダ構成変更

--- _timesheet.py
+++ _timesheet.py
@@ -4,6 +4,18 @@ from pathlib import Path
 from notion_client import Client
 
 class TimeSheet:
-    config = json.load(open('config.json', 'r', encoding='utf-8'))
+    # 設定ファイルの読み込み
+    current_dir = Path(__file__).resolve().parent
+    conf_path   = current_dir / '../config/config.json'
+    config      = json.load(open(conf_path, 'r', encoding='utf-8'))
+
+    # クラス変数
     NOTION_ACCESS_TOKEN = config['NOTION_ACCESS_TOKEN']
-    NOTION_MASTER_ID    = config['NOTION_MASTER_ID']
+    NOTION_MASTER_ID    = config['NOTION_DATABASE_ID']
+
+    def __init__(self):
+        self.client = Client(auth=self.NOTION_ACCESS_TOKEN)
+    
+    def get_all_data(self):
+        db_json = self.client.databases.query(self.NOTION_MASTER_ID)
+        return db_json
---
+++ main.py
@@ -0,0 +1,12 @@
+from _timesheet import Timesheet
+
+# インスタンス生成
+timesheet = Timesheet()
+
+def main():
+    result = timesheet.get_all_data()
+    print(result)
+    return
+
+if __name__ == "__main__":
+    main()
