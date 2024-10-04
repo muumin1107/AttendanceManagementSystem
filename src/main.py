ccdb2f0df4b5bf165824fab87d171ab18b71728e
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-10-04 15:30:42 +0900

    [add] _notionapiの追加

--- src/main.py
+++ src/main.py
@@ -1,59 +1,10 @@
-from lib._timesheet import TimeSheet
-from lib._idpool import IdPool
+from lib._notionapi import NotionAPI
 
-# インスタンス生成
-timesheet = TimeSheet()
-idpool    = IdPool()
+notionapi = NotionAPI()
 
-def add_db(name:str, next_state:str):
-    entry_data = {'name': name, 'next_state': next_state}
-    try:
-        if timesheet.check_data(entry_data=entry_data):
-            timesheet.add_data(entry_data=entry_data)
-            return {"code": 200, "body": "Success"}
-        else:
-            return {"code": 400, "body": "Bad Request"}
-    except Exception as e:
-        return {"code": 500, "body": str(e)}
-
-def remove_db(name:str):
-    try:
-        timesheet.remove_db_data(remove_name=name)
-        return {"code": 200, "body": "Success"}
-    except Exception as e:
-        return {"code": 500, "body": str(e)}
-
-def add_id(id:str, name:str, attribute:str, discription:str):
-    id_data = {'id_num': id, 'name': name, 'attribute': attribute, 'discription': discription}
-    try:
-        if idpool.check_id_data(entry_data=id_data):
-            idpool.add_id_data(entry_data=id_data)
-            return {"code": 200, "body": "Success"}
-        else:
-            return {"code": 400, "body": "Bad Request"}
-    except Exception as e:
-        return {"code": 500, "body": str(e)}
-
-def remove_id(name:str):
-    try:
-        idpool.remove_id_data(remove_name=name)
-        return {"code": 200, "body": "Success"}
-    except Exception as e:
-        return {"code": 500, "body": str(e)}
-
-def search_id(id_num:str):
-    name = idpool.search_id_data(id_num=id_num)
-    try:
-        if name:
-            return {"code": 200, "body": name}
-        else:
-            return {"code": 200, "body": "Not Found"}
-    except Exception as e:
-        return {"code": 500, "body": str(e)}
-
-if __name__ == "__main__":
-    print(search_id(id_num='NDFYUIKJNBVFY'))
-    # print(add_id(id='NDFYUIKJNBVFY', name='原田海斗', attribute='ICカード', discription='ナイスパス'))
-    # print(remove_id(name='原田海斗'))
-    # print(add_db(name='原田海斗', next_state='出勤'))
-    # print(remove_db(name='原田海斗'))
+if __name__ == '__main__':
+    print(notionapi.add_db(name='島田拓斗', next_state='出勤'))
+    # print(notionapi.add_id(id='test', name='test', attribute='ICカード', discription='test'))
+    # print(notionapi.search_id(id_num='test'))
+    # print(notionapi.remove_db(name='test'))
+    # print(notionapi.remove_id(name='test'))
