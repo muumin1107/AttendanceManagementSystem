184459e9dc9277890c863c13a9c812f46c0a572d
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-10-06 20:49:18 +0900

    [add] APIにトークン認証を追加

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 from fastapi import FastAPI
 from lib._notiontools import NotionTools
 from lib._cardreadertools import CardReaderTools
@@ -10,50 +12,62 @@ cardreadertools = CardReaderTools()
 @app.post('/register_id')
 def register_id(id_num:str, name:str, attribute:str, discription:str):
     try:
-        id_num = cardreadertools.encrypt_uid(uid=id_num)
-        if notiontools.add_id(id=id_num, name=name, attribute=attribute, discription=discription):
-            return {'code': 200, 'body': 'ID registration completed.'}
+        result = requests.get(f'https://yleolp2v6n3y2csik6kwvnuioa0zfbik.lambda-url.ap-northeast-1.on.aws/?token={token}').json()
+        if result['code'] == 200 and result['body'] == 'valid':
+            id_num = cardreadertools.encrypt_uid(uid=id_num)
+            if notiontools.add_id(id=id_num, name=name, attribute=attribute, discription=discription):
+                return {'code': 200, 'body': 'ID registration completed.'}
+            else:
+                return {'code': 400, 'body': 'Failed to register ID.'}
         else:
-            return {'code': 400, 'body': 'Failed to register ID.'}
+                return {'code': 400, 'body': 'Invalid token.'}
     except Exception as e:
-            return {'code': 500, 'body': str(e)}
+                return {'code': 500, 'body': str(e)}
 
 # 勤怠データを登録
 @app.post('/register_attendance')
 def register_attendance(id_num:str, next_state:str):
     try:
-        id_num = cardreadertools.encrypt_uid(uid=id_num)
-        name = notiontools.search_id(id_num=id_num)
-        if name:
-            if notiontools.add_db(name=name, next_state=next_state):
-                return {'code': 200, 'body': 'Attendance registration completed.'}
+        result = requests.get(f'https://yleolp2v6n3y2csik6kwvnuioa0zfbik.lambda-url.ap-northeast-1.on.aws/?token={token}').json()
+        if result['code'] == 200 and result['body'] == 'valid':
+            id_num = cardreadertools.encrypt_uid(uid=id_num)
+            name = notiontools.search_id(id_num=id_num)
+            if name:
+                if notiontools.add_db(name=name, next_state=next_state):
+                    return {'code': 200, 'body': 'Attendance registration completed.'}
+                else:
+                    return {'code': 400, 'body': 'Failed to register attendance.'}
             else:
-                return {'code': 400, 'body': 'Failed to register attendance.'}
+                    return {'code': 400, 'body': 'This ID is not registered.'}
         else:
-                return {'code': 400, 'body': 'This ID is not registered.'}
+                    return {'code': 400, 'body': 'Invalid token.'}
     except Exception as e:
-                return {'code': 500, 'body': str(e)}
+                    return {'code': 500, 'body': str(e)}
 
 # IDを削除
 @app.post('/remove_data')
-def remove_data(id_num:str, mode:str, name:str):
+def remove_data(token:str, id_num:str, mode:str, name:str):
     try:
-        id_num = cardreadertools.encrypt_uid(uid=id_num)
-        is_user = notiontools.search_id(id_num=id_num)
-        if is_user == name and mode == 'id':
-            if notiontools.remove_id(name=name):
-                return {'code': 200, 'body': 'ID deletion completed.'}
+        result = requests.get(f'https://yleolp2v6n3y2csik6kwvnuioa0zfbik.lambda-url.ap-northeast-1.on.aws/?token={token}').json()
+        if result['code'] == 200 and result['body'] == 'valid':
+            id_num = cardreadertools.encrypt_uid(uid=id_num)
+            is_user = notiontools.search_id(id_num=id_num)
+            if is_user == name and mode == 'id':
+                if notiontools.remove_id(name=name):
+                    return {'code': 200, 'body': 'ID deletion completed.'}
+                else:
+                    return {'code': 400, 'body': 'Failed to delete ID.'}
+            elif is_user == name and mode == 'db':
+                if notiontools.remove_db(name=name):
+                    return {'code': 200, 'body': 'Attendance data deletion completed.'}
+                else:
+                    return {'code': 400, 'body': 'Failed to delete attendance data.'}
             else:
-                return {'code': 400, 'body': 'Failed to delete ID.'}
-        elif is_user == name and mode == 'db':
-            if notiontools.remove_db(name=name):
-                return {'code': 200, 'body': 'Attendance data deletion completed.'}
-            else:
-                return {'code': 400, 'body': 'Failed to delete attendance data.'}
+                    return {'code': 400, 'body': 'Name is not found. / mode is invalid.'}
         else:
-                return {'code': 400, 'body': 'Name is not found. / mode is invalid.'}
+                    return {'code': 400, 'body': 'Invalid token.'}
     except Exception as e:
-                return {'code': 500, 'body': str(e)}
+                    return {'code': 500, 'body': str(e)}
 
 @app.get('/')
 def root():
