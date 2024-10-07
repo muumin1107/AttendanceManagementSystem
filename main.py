c2bf1a373a5bbc7dd3e6f6a42b90f58b52bd7d4b
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-10-07 12:50:22 +0900

    [add] 引数の変更

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import requests
-
 from fastapi import FastAPI
 from lib._notiontools import NotionTools
 from lib._cardreadertools import CardReaderTools
@@ -10,26 +8,20 @@ cardreadertools = CardReaderTools()
 
 # IDプールにID情報を登録
 @app.post('/register_id')
-def register_id(token:str, id_num:str, name:str, attribute:str, discription:str):
+def register_id(id_num:str, name:str, attribute:str, discription:str):
     try:
-        # result = requests.get(f'https://yleolp2v6n3y2csik6kwvnuioa0zfbik.lambda-url.ap-northeast-1.on.aws/?token={token}').json()
-        # if result['result'] == 'valid':
         id_num = cardreadertools.encrypt_uid(uid=id_num)
         if notiontools.add_id(id=id_num, name=name, attribute=attribute, discription=discription):
             return {'code': 200, 'body': 'ID registration completed.'}
         else:
             return {'code': 400, 'body': 'Failed to register ID.'}
-        # else:
-        #         return {'code': 400, 'body': 'Invalid token.'}
     except Exception as e:
             return {'code': 500, 'body': str(e)}
 
 # 勤怠データを登録
 @app.post('/register_attendance')
-def register_attendance(token:str, id_num:str, next_state:str):
+def register_attendance(id_num:str, next_state:str):
     try:
-        # result = requests.get(f'https://yleolp2v6n3y2csik6kwvnuioa0zfbik.lambda-url.ap-northeast-1.on.aws/?token={token}').json()
-        # if result['result'] == 'valid':
         id_num = cardreadertools.encrypt_uid(uid=id_num)
         name = notiontools.search_id(id_num=id_num)
         if name:
@@ -39,17 +31,13 @@ def register_attendance(token:str, id_num:str, next_state:str):
                 return {'code': 400, 'body': 'Failed to register attendance.'}
         else:
                 return {'code': 400, 'body': 'This ID is not registered.'}
-        # else:
-        #             return {'code': 400, 'body': 'Invalid token.'}
     except Exception as e:
                 return {'code': 500, 'body': str(e)}
 
 # IDを削除
 @app.post('/remove_data')
-def remove_data(token:str, id_num:str, mode:str, name:str):
+def remove_data(id_num:str, mode:str, name:str):
     try:
-        # result = requests.get(f'https://yleolp2v6n3y2csik6kwvnuioa0zfbik.lambda-url.ap-northeast-1.on.aws/?token={token}').json()
-        # if result['result'] == 'valid':
         id_num = cardreadertools.encrypt_uid(uid=id_num)
         is_user = notiontools.search_id(id_num=id_num)
         if is_user == name and mode == 'id':
@@ -64,8 +52,6 @@ def remove_data(token:str, id_num:str, mode:str, name:str):
                 return {'code': 400, 'body': 'Failed to delete attendance data.'}
         else:
                 return {'code': 400, 'body': 'Name is not found. / mode is invalid.'}
-        # else:
-        #             return {'code': 400, 'body': 'Invalid token.'}
     except Exception as e:
                 return {'code': 500, 'body': str(e)}
 
