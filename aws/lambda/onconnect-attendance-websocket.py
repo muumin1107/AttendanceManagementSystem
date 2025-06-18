cfd11c13ee08aff3d3f7bebf4c6526a02252521d
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-06-18 18:10:00 +0900

    [add] WebSocketの接続を認証なしに

--- aws/lambda/onconnect-attendance-websocket.py
+++ aws/lambda/onconnect-attendance-websocket.py
@@ -1,27 +1,19 @@
 import os
 import boto3
-import time
+import datetime
 
 # 環境変数を取得
 CONNECTION_TABLE_NAME = os.environ['CONNECTION_TABLE_NAME']
-VALID_API_KEY         = os.environ['VALID_API_KEY']
 
 dynamodb = boto3.resource("dynamodb")
 table    = dynamodb.Table(CONNECTION_TABLE_NAME)
 
 def lambda_handler(event, context):
     try:
-        # クライアントから送られてきたAPIキーをクエリパラメータから取得
-        query_params = event.get('queryStringParameters', {})
-        client_api_key = query_params.get('apiKey')
-        # 正しいAPIキーと一致するか検証
-        if client_api_key != VALID_API_KEY:
-            print("API Key validation failed.")
-            return {"statusCode": 403, "body": "Forbidden"}
-
         # 既存の接続処理
         connection_id = event["requestContext"]["connectionId"]
-        timestamp = int(time.time())
+        # 日本時間
+        timestamp     = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime("%Y-%m-%d %H:%M:%S"))
 
         # DynamoDBに保存
         table.put_item(Item={
