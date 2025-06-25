700b2e80853dce8cfe30c1f955bc802b3dad2dee
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-06-25 14:07:41 +0900

    [add] Cognitoログイン機能

--- attendance-viewer/src/web/frontend/src/App.tsx
+++ attendance-viewer/src/web/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 import LoadingPage from './pages/LoadingPage/LoadingPage';
 import ErrorPage   from './pages/ErrorPage/ErrorPage';
@@ -8,12 +9,14 @@ import HomePage    from './pages/HomePage/HomePage';
 function App() {
 	return (
 		<Router>
-			<Routes>
-				<Route path="/"      element={<LoadingPage />} />
-				<Route path="/views" element={<HomePage />} />
-				<Route path="/error" element={<ErrorPage />} />
-				<Route path="*"      element={<LoadingPage />} />
-			</Routes>
+			<Authenticator>
+				<Routes>
+					<Route path="/"      element={<LoadingPage />} />
+					<Route path="/views" element={<HomePage />} />
+					<Route path="/error" element={<ErrorPage />} />
+					<Route path="*"      element={<LoadingPage />} />
+				</Routes>
+			</Authenticator>
 		</Router>
 	);
 }
