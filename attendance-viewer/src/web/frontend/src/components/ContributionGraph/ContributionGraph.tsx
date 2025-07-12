a31726e2f2eeae0d35d56fdccb996672c0ca9eac
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-07-12 18:15:39 +0900

    [add] デザインを修正

--- attendance-viewer/src/web/frontend/src/components/ContributionGraph/ContributionGraph.tsx
+++ attendance-viewer/src/web/frontend/src/components/ContributionGraph/ContributionGraph.tsx
@@ -59,9 +59,13 @@ return (
         </div>
         <div className="days-and-cells">
         <div className="days-col">
+            <span>Sun</span>
             <span>Mon</span>
+            <span>Tue</span>
             <span>Wed</span>
+            <span>Thu</span>
             <span>Fri</span>
+            <span>Sat</span>
         </div>
         <div className="cells-grid">
             {calendarDays.map((date, i) => {
