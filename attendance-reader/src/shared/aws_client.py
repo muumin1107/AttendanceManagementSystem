13e651a7fc4805f73f91d75af0400127ce7e2bfe
muumin1107 <75819611+muumin1107@users.noreply.github.com>
2025-10-03 10:42:21 +0900

    [add] 最新バージョン

--- attendance-reader/src/shared/aws_client.py
+++ attendance-reader/src/shared/aws_client.py
@@ -11,12 +11,10 @@ from shared.error_handler  import ErrorHandler
 # 初期化
 load_dotenv()
 
-# 一時的な認証情報を取得する
 cognito_client = CognitoClient(
     identity_pool_id = os.getenv("IDENTITY_POOL_ID"),
     region_name      = os.getenv("REGION_NAME")
 )
-cognito_creds = cognito_client.get_cognito_credentials()
 
 # SecretManagerからAPIキーを取得する
 # sm_client = SMClient(
@@ -28,22 +26,24 @@ cognito_creds = cognito_client.get_cognito_credentials()
 # )
 # sm_creds = sm_client.get_api_key()
 
-
-api_client = APIClient(
-    access_key    = cognito_creds["access_key"],
-    secret_key    = cognito_creds["secret_key"],
-    session_token = cognito_creds["session_token"],
-    resource_id   = os.getenv("RESOURCE_ID"),
-    service_name  = os.getenv("SERVICE_NAME"),
-    region_name   = os.getenv("REGION_NAME"),
-    x_api_key     = os.getenv("X_API_KEY")
-    # x_api_key     = sm_creds["x-api-key"]
-)
-
 logger = ErrorHandler(log_file=str(LOG_PATHS["aws_client"]))
 
 def register_user(id: str, name: str, grade:str):
     """AWS APIでユーザー登録"""
+    # 一時的な認証情報を取得する
+    cognito_creds = cognito_client.get_cognito_credentials()
+    # APIクライアントを初期化
+    api_client = APIClient(
+        access_key    = cognito_creds["access_key"],
+        secret_key    = cognito_creds["secret_key"],
+        session_token = cognito_creds["session_token"],
+        resource_id   = os.getenv("RESOURCE_ID"),
+        service_name  = os.getenv("SERVICE_NAME"),
+        region_name   = os.getenv("REGION_NAME"),
+        x_api_key     = os.getenv("X_API_KEY")
+        # x_api_key     = sm_creds["x-api-key"]
+    )
+
     data = {
         "id"   : Codec.base64_encode(id),
         "name" : Codec.base64_encode(name),
@@ -59,6 +59,20 @@ def register_user(id: str, name: str, grade:str):
 
 def register_attendance(id: str, status: str):
     """AWS APIで勤怠登録"""
+    # 一時的な認証情報を取得する
+    cognito_creds = cognito_client.get_cognito_credentials()
+    # APIクライアントを初期化
+    api_client = APIClient(
+        access_key    = cognito_creds["access_key"],
+        secret_key    = cognito_creds["secret_key"],
+        session_token = cognito_creds["session_token"],
+        resource_id   = os.getenv("RESOURCE_ID"),
+        service_name  = os.getenv("SERVICE_NAME"),
+        region_name   = os.getenv("REGION_NAME"),
+        x_api_key     = os.getenv("X_API_KEY")
+        # x_api_key     = sm_creds["x-api-key"]
+    )
+
     data = {
         "id"    : Codec.base64_encode(id),
         "status": Codec.base64_encode(status)
@@ -70,31 +84,3 @@ def register_attendance(id: str, status: str):
     except Exception as e:
         logger.log_error(f"Attendance registration failed: {e}")
         raise
-
-
-# if __name__ == '__main__':
-#     """
-#     このスクリプトが直接実行された場合に、以下の処理を行う
-#     (例: python -m shared.aws_client)
-#     """
-#     try:
-#         print("--- Main process started ---")
-        
-#         # 例として、勤怠登録APIを呼び出す
-#         user_id = "92ac2e7ad130b70c0b98bf31c86fb077c60401be443f36f32c18df949cc45136"
-#         status = "clock_in"
-        
-#         print(f"Attempting to register attendance for user: {user_id}")
-#         response = register_attendance(id=user_id, status=status)
-        
-#         print("--- API call successful ---")
-#         print("Status Code:", response.get("statusCode") or "N/A")
-#         print("Response Body:", response.get("body") or "N/A")
-
-#     except Exception as e:
-#         # logger を使ってエラーを記録することも可能
-#         logger.log_error(f"An error occurred in the main process: {e}")
-#         print(f"An error occurred in the main process: {e}")
-    
-#     finally:
-#         print("--- Script finished ---")
